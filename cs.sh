8ed0ddea2dc75f92611debffcaa61fef656d3f55
voidspace01 <voidspace01@gmail.com>
2019-04-03 13:10:13 +0300

    change cs.sh

--- cs.sh
+++ cs.sh
@@ -3,6 +3,4 @@ touch cscope.out
 find ./ -name "*.c" -o -name "*.pc" -o -name "*.h" > cscope.files
 cscope -b -q -i cscope.files
 rm -f ./TAGS
-find . -iname "*.c" -print -or -iname "*.h" -print -or -iname "*.pc" -print | etags -o TAGS -
-
-
+find . -iname "*.c" -print -or -iname "*.h" -print -or -iname "*.pc" -print | xargs etags -o TAGS
